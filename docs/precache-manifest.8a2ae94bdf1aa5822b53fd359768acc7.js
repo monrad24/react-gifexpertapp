20363a972ce8cd08b97ea7da1164d77cf15e7100
Claudio Garcia Iglesia <claudiogarciaiglesia@gmail.com>
2020-08-09 01:23:29 -0300

    add docs

---
+++ docs/precache-manifest.8a2ae94bdf1aa5822b53fd359768acc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe9bad7a1eeb0cac42afc36f231b11b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27b32250dda1f691c425",
+    "url": "/static/css/main.c71f280f.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b32250dda1f691c425",
+    "url": "/static/js/main.850fd7b4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
